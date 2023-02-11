61b9ca3d62715440a6062c76a9004c7bdf7358a6
andreiChabatul <andreichabatul@gmail.com>
2023-02-11 15:08:34 +0300

    fix: add style page

--- src/pages/achievements/index.tsx
+++ src/pages/achievements/index.tsx
@@ -4,8 +4,19 @@ import './index.css';
 const AchievementsPage = () => {
     return (
         <MainLayout>
-            <div>
-            AchievementsPage
+            <div className='achievements-container'>
+                <div className='achievements-container_title'>
+                    <h1 className='achievements-title_text'>Достижения</h1>
+                </div>
+                <div className='achievements-container_item'>
+                    <img src="" alt="" />
+                    <div>
+                        <p>Название</p>
+                        <p>Описание</p>
+                        <p>Набранно баллов</p>
+                    </div>
+                </div>
+
             </div>
         </MainLayout >
     );
