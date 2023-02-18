a9325ba33089ebb18d2e25b51becb2518c7fef55
Igor Antonenko <i_antonenko@lesta.group>
2023-02-18 04:49:42 +0300

    update: change server

--- src/configs/AppConfig.ts
+++ src/configs/AppConfig.ts
@@ -1,5 +1,5 @@
 const appConfig = {
-  apiUrl: 'http://88.150.174.11:5003/api',
+  apiUrl: 'https://tododone4.onrender.com/api',
   //apiUrl: 'http://localhost:5000/api',
   //apiUrl: 'https://rs-clone-server-production.up.railway.app/api',
 };
