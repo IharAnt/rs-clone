223d43d803ba63b24b21ad0ac3ccb7d3158a18fe
Igor Antonenko <i_antonenko@lesta.group>
2023-02-19 02:20:54 +0300

    feat: add search param to getRating

--- src/services/RatingService.ts
+++ src/services/RatingService.ts
@@ -2,7 +2,6 @@ import RatingSortType from '../types/enums/RatingSortEnum';
 import OrderType from '../types/enums/OrderEnum';
 import { IPaginationResponse } from '../types/interfaces/IPagination';
 import { IRating } from '../types/interfaces/IRating';
-import ratings from '../data/Ratings';
 import { IAchievement } from '../types/interfaces/IAchievement';
 import apiClient from '../api/ApiClient';
 import Webrequest from '../helpers/WebRequest';
@@ -14,11 +13,13 @@ export default class RatingService {
     page: number,
     limit = 10,
     sort?: RatingSortType,
-    order?: OrderType
+    order?: OrderType,
+    search = ''
   ): Promise<IPaginationResponse<IRating>> {
     let query = Webrequest.generateQuery({
       _page: page,
       _limit: limit,
+      _search: search,
     });
     if (sort && order && sort !== RatingSortType.empty && order !== OrderType.empty) {
       query = Webrequest.generateQuery({
@@ -26,6 +27,7 @@ export default class RatingService {
         _limit: limit,
         _sort: sort,
         _order: order,
+        _search: search,
       });
     }
     const response = await apiClient.get<IPaginationResponse<IRating>>(`${RatingService.ratingPath}${query}`);
