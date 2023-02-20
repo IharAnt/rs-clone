554a570e6f13469b60733ffa6813e778450fc14e
Stepan Zubik <stteppa9092@gmail.com>
2023-02-20 10:54:09 +0300

    feat: add navigate to mainpage on logout

--- src/pages/motivators/index.tsx
+++ src/pages/motivators/index.tsx
@@ -3,13 +3,17 @@ import { useState } from 'react';
 import MotivatorsNavigation from '../../components/motivatorsNavigation';
 import MotivatorsContent from '../../components/motivatorsContent';
 import MainLayout from '../../layouts/main';
+import { useAppSelector } from '../../store';
+import { Navigate } from 'react-router-dom';
 
 const MotivatorsPage = () => {
 
   const [content, setContent] = useState('myTasks');
+  const isLogin = useAppSelector((state) => state.appState.isLogin)
 
   return (
     <MainLayout>
+      {!isLogin ? <Navigate to={"/"} /> : ''} 
       <div className='motivators'>
         <MotivatorsNavigation content={content} setContent={setContent} />
         <MotivatorsContent content={content} setContent={setContent} />
