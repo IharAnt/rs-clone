8edac54fda921ecb8db2eec558acbb6c4d73adda
Andrei Chabatul <andreichabatul@gmail.com>
2023-02-24 10:50:54 +0300

    fix: add button auth, when islogin = false

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,11 +1,14 @@
-import React, { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { NAVIGATION_ROUTES } from '../../routes';
 import { Link, useLocation } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { pageChange } from '../../store/appStore/sliceApp/slice';
+import Modal from '../modal';
+import Authorization from '../authorization';
 
 const Navigation: FC = () => {
-  const actualPage = useAppSelector((state) => state.appState.activePage);
+  const { isLogin, activePage } = useAppSelector((state) => state.appState);
+  const [modalAuthorization, setModalAuthorization] = useState(false);
   const location = useLocation().pathname;
   const dispatch = useAppDispatch();
 
@@ -14,24 +17,33 @@ const Navigation: FC = () => {
   }, [dispatch, location]);
 
   return (
-    <ul className="flex gap-4 header-navigation">
-      {NAVIGATION_ROUTES.map((route) => {
-        return (
-          <li key={route.name} className="list-none">
-            <Link
-              className={`text-indigo-800 ${
-                actualPage === route.path
-                  ? 'font-bold cursor-default'
-                  : 'border-indigo-800 border-solid hover:border-b-2'
-              }`}
-              to={route.path}
-            >
-              {route.name}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <Modal isOpen={modalAuthorization} setModal={setModalAuthorization}>
+        <Authorization />
+      </Modal>
+      <ul className="flex gap-4 header-navigation">
+        {NAVIGATION_ROUTES.map((route) => {
+          return (
+            <li key={route.name} className="list-none">
+              {(!isLogin && route.path) !== '/motivators' ?
+                <Link
+                  className={`text-indigo-800 ${activePage === route.path
+                    ? 'font-bold cursor-default'
+                    : 'border-indigo-800 border-solid hover:border-b-2'
+                    }`}
+                  to={route.path}
+                >
+                  {route.name}
+                </Link> : <button
+                  className='text-indigo-800 border-indigo-800 border-solid hover:border-b-2'
+                  onClick={() => setModalAuthorization(true)}
+                >{route.name}</button>
+              }
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
