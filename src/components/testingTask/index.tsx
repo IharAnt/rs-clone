16057e567279f1f05f6eb491f5ded15afa3a4295
Stepan Zubik <stteppa9092@gmail.com>
2023-02-27 12:05:58 +0300

    feat: add info modal on test task

--- src/components/testingTask/index.tsx
+++ src/components/testingTask/index.tsx
@@ -1,6 +1,6 @@
 import { props } from './types';
 import './style.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactNode } from 'react';
 import { useAppDispatch } from '../../store';
 import { updateModalTask, updateModalValue } from '../../store/motivatorsStore/sliceTasks/tasks';
 import UserService from '../../services/UserService';
@@ -19,32 +19,41 @@ export default function TestingTask({ task }: props) {
     fn();
   }, [dispatch]);
 
-  let shortReport = task.taskReport;
+  const doShortString = (string: string) => {
+    let shortString = string.slice(0, 80);
+    shortString = shortString.slice(0, shortString.lastIndexOf(' ')).concat('...');
+    return string!.length > 80 ? shortString : string;
+  };
 
-  if (shortReport!.length > 80) {
-    shortReport = task.taskReport!.slice(0, 80);
-    shortReport = shortReport.slice(0, shortReport.lastIndexOf(' ')).concat('...');
-  }
+  const linkify = (node: ReactNode) => (
+    <Linkify
+      componentDecorator={(decoratedHref, decoratedText, key) => (
+        <a className="linkify" target="blank" href={decoratedHref} key={key}>
+          {decoratedText}
+        </a>
+      )}
+    >
+      {node}
+    </Linkify>
+  );
 
   return (
-    <div className="motivatorsTesting__item motivatorsTesting-grid">
+    <div
+      className="motivatorsTesting__item motivatorsTesting-grid"
+      onClick={(e) => {
+        e.stopPropagation();
+        dispatch(updateModalValue('info'));
+        dispatch(updateModalTask(task));
+      }}
+    >
       <img className="motivatorsTesting__avatar" src={avatar} alt="avatar" />
       <h2 className="motivatorsTesting__name">{task.summary}</h2>
-      <div className="motivatorsTesting__description">{task.description}</div>
-      <div className="motivatorsTesting__report">
-        <Linkify
-          componentDecorator={(decoratedHref, decoratedText, key) => (
-            <a className="linkify" target="blank" href={decoratedHref} key={key}>
-              {decoratedText}
-            </a>
-          )}
-        >
-          {shortReport}
-        </Linkify>
-      </div>
+      <div className="motivatorsTesting__description">{linkify(doShortString(task.description as string))}</div>
+      <div className="motivatorsTesting__report">{linkify(doShortString(task.taskReport as string))}</div>
       <button
         className="motivators-block motivatorsTask__btn motivatorsTesting__btn"
-        onClick={() => {
+        onClick={(e) => {
+          e.stopPropagation();
           dispatch(updateModalValue('test'));
           dispatch(updateModalTask(task));
         }}
